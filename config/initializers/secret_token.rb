4d8a514041cfa53b0f7ca1454baa09e6ee3a179d
Guanglin Du <guanglin.du@gmail.com>
2014-10-21 13:41:51 +0800

    IPAMS Scaffold, 1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ipams::Application.config.secret_key_base = 'ae986f1f93b1c7b4d5a7c4354f870f0cad4d1893d1d14b9157c1d11cd08f79f349e6f42fe0116f15f575de3f815202c4e58d7fe857d467e0a79b69eab6692582'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ipams::Application.config.session_store :cookie_store, key: '_ipams_session'
