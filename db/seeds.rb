d64c495a8fa449f518d82acd3a2e39bdb0a6a030
GuanglinDu <guanglin.du@gmail.com>
2014-10-26 15:29:40 +0800

    Correct the one-to-many relationship between table lans & table vlans in the dia file. Create the test dat for table lans in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Test data for table lans
+
+# Test data for table valns
+
+# Test data for table departments
+
+# Test data for table addresses
+
+# Test data for table users
+
+
+# Test data for table reserved_addresses
+
+# Test data for table histories
+
+ 
+Lan.delete_all
+Lan.create!(:lan_number => 1, :lan_name => "Legacy LAN",
+  :lan_description => %{<p>The traditional LAN with 4-C LANs.</p>})
+Lan.create!(:lan_number => 2, :lan_name => "Datacenter LAN",
+  :lan_description => %{<p>The new LAN with 1-C LAN inside the DC building.</p>})
+
