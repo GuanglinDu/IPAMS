b80928ddbd38442888af6d0e94d7fae4542a174b
GuanglinDu <guanglin.du@gmail.com>
2014-10-29 22:39:51 +0800

    Create app/views/vlans/show.html.erb
    1. Fix several small html code bugs concerning the views.
    2. Implement the Destroy action of a VLAN record.
    3. Implementing the Show and Edit actions of a VLAN record.

--- app/controllers/vlans_controller.rb
+++ app/controllers/vlans_controller.rb
@@ -8,4 +8,17 @@ class VlansController < ApplicationController
     redirect_to lan_path(@lan)
   end
 
+  def show
+    @lan = Lan.find(params[:lan_id])
+    @vlan = @lan.vlans.find(params[:id])
+    
+  end
+
+  # Destroy a VLAN record
+  def destroy
+    @lan = Lan.find(params[:lan_id])
+    @vlan = @lan.vlans.find(params[:id])
+    @vlan.destroy
+    redirect_to lan_path(@lan)
+  end
 end
