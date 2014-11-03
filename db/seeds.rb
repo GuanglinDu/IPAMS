4632b0638ecd67c7dd146bf9f1f093898f55ecdc
GuanglinDu <guanglin.du@gmail.com>
2014-11-03 22:55:48 +0800

    Seed child table vlans and implement show/edit of a VLAN

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,28 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Test data for table lans
+Vlan.delete_all
 Lan.delete_all
-Lan.create!(:lan_number => 1, :lan_name => "Legacy LAN",
+
+lan1 = Lan.create!(:lan_number => 1, :lan_name => "Legacy LAN",
   :lan_description => %{<p>The traditional LAN with 4-C LANs each.</p>})
-Lan.create!(:lan_number => 2, :lan_name => "Datacenter LAN",
+lan2 = Lan.create!(:lan_number => 2, :lan_name => "Datacenter LAN",
   :lan_description => %{<p>The new LAN with 1-C LAN each inside the DC building.</p>})
 
 # Test data for table valns
+vlan11_of_lan1 = Vlan.create!(lan_id: lan1.id, vlan_number: 11, vlan_name: "VLAN_11", subnet_mask: "255.255.255.0",
+  gateway: "192.168.0.254", static_ip_start: "192.168.0.0", static_ip_end: "192.168.0.255", 
+  vlan_description: "Storey 1 of the Main Buiding")
+vlan12_of_lan1 = Vlan.create!(lan_id: lan1.id, vlan_number: 12, vlan_name: "VLAN_12", subnet_mask: "255.255.255.0",
+  gateway: "192.168.1.254", static_ip_start: "192.168.1.0", static_ip_end: "192.168.1.255", 
+  vlan_description: "Storey 2 of the Main Buiding")
+
+vlan21_of_lan2 = Vlan.create!(lan_id: lan2.id, vlan_number: 21, vlan_name: "VLAN_21", subnet_mask: "255.255.255.0",
+  gateway: "192.168.2.254", static_ip_start: "192.168.2.0", static_ip_end: "192.168.2.255", 
+  vlan_description: "Storey 1 of Buiding DC")
+vlan22_of_lan2 = Vlan.create!(lan_id: lan2.id, vlan_number: 22, vlan_name: "VLAN_22", subnet_mask: "255.255.255.0",
+  gateway: "192.168.3.254", static_ip_start: "192.168.3.0", static_ip_end: "192.168.3.255", 
+  vlan_description: "Storey 2 of Buiding DC")
 
 # Test data for table departments
 
