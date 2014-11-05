1d7dabb81c23a4b3d51bdb9cfba59bdf04f0c303
Guanglin Du <guanglin.du@gmail.com>
2014-11-05 08:03:54 +0800

    Improve the LAN and VLAN show.html.erb files

--- db/migrate/20141028070538_create_vlans.rb
+++ db/migrate/20141028070538_create_vlans.rb
@@ -1,12 +1,12 @@
 class CreateVlans < ActiveRecord::Migration
   def change
     create_table :vlans do |t|
-      t.integer :vlan_number
-      t.string :vlan_name
+      t.integer :vlan_number, unique: true
+      t.string :vlan_name, unique: true
       t.string :subnet_mask
-      t.string :gateway
-      t.string :static_ip_start
-      t.string :static_ip_end
+      t.string :gateway, unique: true
+      t.string :static_ip_start, unique: true
+      t.string :static_ip_end, unique: true
       t.text :vlan_description
       t.references :lan, index: true
 
