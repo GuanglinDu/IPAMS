996ee29d94a81b7a03c85e7475bd81a034bb5013
GuanglinDu <guanglin.du@gmail.com>
2014-11-07 08:57:13 +0800

    Add comments at the start of the 3 *.perpective_controller.rb files

--- app/controllers/vlans_perspective_controller.rb
+++ app/controllers/vlans_perspective_controller.rb
@@ -1,3 +1,5 @@
+# This perspective is used to show the VLANs' info directly
+# without starting from the LANs which parent VLANs. 
 class VlansPerspectiveController < ApplicationController
   def index
   end
