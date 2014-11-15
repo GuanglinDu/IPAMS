a9585e06fa814ad4cceff502b8126c31d64e7d69
Guanglin Du <guanglin.du@gmail.com>
2014-11-15 13:58:35 +0800

    Implementing the VLANs view (perspective)

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,17 +20,17 @@ lan2 = Lan.create!(:lan_number => 2, :lan_name => "Datacenter LAN",
 # Test data for table valns
 vlan11_of_lan1 = Vlan.create!(lan_id: lan1.id, vlan_number: 11, vlan_name: "VLAN_11", subnet_mask: "255.255.255.0",
   gateway: "192.168.0.254", static_ip_start: "192.168.0.0", static_ip_end: "192.168.0.255", 
-  vlan_description: "Storey 1 of the Main Buiding")
+  vlan_description: "Storey 1 of the Main Building")
 vlan12_of_lan1 = Vlan.create!(lan_id: lan1.id, vlan_number: 12, vlan_name: "VLAN_12", subnet_mask: "255.255.255.0",
   gateway: "192.168.1.254", static_ip_start: "192.168.1.0", static_ip_end: "192.168.1.255", 
-  vlan_description: "Storey 2 of the Main Buiding")
+  vlan_description: "Storey 2 of the Main Building")
 
 vlan21_of_lan2 = Vlan.create!(lan_id: lan2.id, vlan_number: 21, vlan_name: "VLAN_21", subnet_mask: "255.255.255.0",
   gateway: "192.168.2.254", static_ip_start: "192.168.2.0", static_ip_end: "192.168.2.255", 
-  vlan_description: "Storey 1 of Buiding DC")
+  vlan_description: "Storey 1 of Building DC")
 vlan22_of_lan2 = Vlan.create!(lan_id: lan2.id, vlan_number: 22, vlan_name: "VLAN_22", subnet_mask: "255.255.255.0",
   gateway: "192.168.3.254", static_ip_start: "192.168.3.0", static_ip_end: "192.168.3.255", 
-  vlan_description: "Storey 2 of Buiding DC")
+  vlan_description: "Storey 2 of Building DC")
 
 # Test data for table departments
 User.delete_all
