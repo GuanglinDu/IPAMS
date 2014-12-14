e93fcb004706ec2943980b3a086a7530c3d88ed9
GuanglinDu <guanglin.du@gmail.com>
2014-12-14 22:09:59 +0800

    Implementing import/export feature

---
+++ app/controllers/import_export_controller.rb
@@ -0,0 +1,9 @@
+class ImportExportController < ApplicationController
+  def import
+    #Vlan.import(params[:file])
+    #redirect_to vlans_url, notice: "VLANs imported."
+  end
+
+  def export
+  end
+end
--- app/controllers/vlans_controller.rb
+++ app/controllers/vlans_controller.rb
@@ -76,18 +76,4 @@ class VlansController < ApplicationController
         :subnet_mask, :gateway, :vlan_description)
     end
 
-    # Helper methods accessiable from the views. See
-    # http://api.rubyonrails.org/classes/AbstractController/Helpers/ClassMethods.html#method-i-helper_method
-    def find_lan_name(id)
-      name = "unnamed"
-      begin
-        lan = Lan.find(id)
-        name = lan.lan_name
-      rescue ActiveRecord::RecordNotFound
-        name = "RecordNotFound"
-      end
-      name
-    end
-   
-    helper_method :find_lan_name
 end
