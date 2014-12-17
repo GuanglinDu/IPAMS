fe092333f8b0793adf42b9a4a28695f8e9017e67
Guanglin Du <guanglin.du@gmail.com>
2014-12-17 11:44:03 +0800

    Add validation

--- app/models/lan.rb
+++ app/models/lan.rb
@@ -1,3 +1,7 @@
 class Lan < ActiveRecord::Base
+  validates :lan_number, presence: true
+  validates :lan_name, :lan_description, presence: true, length: { minimum: 5 }
+  validates :lan_number, :lan_name, uniqueness: true
+
   has_many :vlans, dependent: :destroy
 end
