ba50d51d1051f9fabed121c0c9a77b18c7afa19a
GuanglinDu <guanglin.du@gmail.com>
2014-12-17 22:31:27 +0800

    Resolve the i18n warning in i18n.rb

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,4 +1,6 @@
 #encoding: utf-8
+I18n.enforce_available_locales = false 
+
 # Default locale
 I18n.default_locale = :en
 #I18n.default_locale = :zh_CN
@@ -8,3 +10,4 @@ LANGUAGES = [
   ['English', 'en'],
   ["Simplified Chinese", 'zh_CN']
 ]
+
