27b2c6ae41bb3836c8ae2e49c204734e68336743
GuanglinDu <guanglin.du@gmail.com>
2014-12-23 23:03:19 +0800

    Supplementing vlan model unit tests

--- test/models/vlan_test.rb
+++ test/models/vlan_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class VlanTest < ActiveSupport::TestCase
-  test "VLAN attributes must all be present (non-blank)" do
-    vlan = Lan.new
+  test "A Vlan object with default empty attributes is invalid" do
+    vlan = Vlan.new
     assert_not vlan.valid?, "should be invalid with all the attributes empty"
   end
 
@@ -23,7 +23,7 @@ class VlanTest < ActiveSupport::TestCase
     vlan1.vlan_number = 4097
     assert_not vlan1.valid?, "Any number outside 1..4096 should be an invalid vlan_number"
 
-    # Should be valid
+    # vlan_nubmer between 1 and 4096 should be valid
     vlan1.vlan_number = 88
     assert vlan1.valid?, "Any number within 1..4096, inclusive, should be an valid vlan_number"
   end
@@ -35,8 +35,14 @@ class VlanTest < ActiveSupport::TestCase
     assert_not_nil vlan2.lan_id, "The FK of VLAN fixture two should be found"
   end
 
- # Netmask tests
- # Gateway tests
- # static_ip_start tests
- # static_ip_end tests
+  # Non-black netmask test (not unique)
+  test "Subnet mask should be non-blank" do
+    vlan2 = vlans(:two) # pass the fixture to a local varible to modify
+    vlan2.subnet_mask = nil
+    assert vlan2.invalid?, "Bland subnet mask should be invalid"
+  end
+
+  # Non-blank & unique gateway tests
+  # Non-blank & unique static_ip_start tests
+  # Non-blank & unique static_ip_end tests
 end
