873011947846f05f5fa452cbbe3073d7f0b1addc
GuanglinDu <guanglin.du@gmail.com>
2014-12-23 23:03:19 +0800

    Supplementing vlan model unit tests

--- app/models/vlan.rb
+++ app/models/vlan.rb
@@ -1,7 +1,7 @@
 class Vlan < ActiveRecord::Base
   require 'csv'
 
-  validates :vlan_number, :vlan_name,:vlan_description, presence: true
+  validates :vlan_number, :vlan_name, :subnet_mask, :vlan_description, presence: true
   # Valid VLAN number is only between 1..4096 (an Range object)
   validates :vlan_number, inclusion: { in: 1..4096 }
   validates :gateway, :static_ip_start, :static_ip_end, presence: true, uniqueness: true
