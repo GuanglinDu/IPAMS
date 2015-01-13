bd841ed77d861f00fb95e44737e929745ec54096
Guanglin Du <guanglin.du@gmail.com>
2015-01-13 14:27:30 +0800

    Add password reset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,20 @@
 class ApplicationController < ActionController::Base
   include Pundit
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   # Select locale according to user's selection
   before_action :set_i18n_locale_from_params, :authenticate_system_user!
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Used to reset the password
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) { |u| 
+      u.permit(:password, :password_confirmation, :current_password) 
+    }
+  end
 
   protected
 
