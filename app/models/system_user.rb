0558d2dcc1c17206c89342605a8c6a5fa4fa029f
GuanglinDu <guanglin.du@gmail.com>
2015-01-13 21:32:31 +0800

    Implementing a role-based authorization

--- app/models/system_user.rb
+++ app/models/system_user.rb
@@ -1,4 +1,13 @@
+# See https://github.com/RailsApps/rails-devise-roles/
 class SystemUser < ActiveRecord::Base
+  ROLES = { nobody: 0, guest: 1, operator: 2, expert: 3, admin: 4 }
+  #enum role: [:nobody, :guest, :operator, :expert, :admin] # ~> Rails 4.1
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    @role = 0
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
