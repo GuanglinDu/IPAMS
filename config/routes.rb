683bf9144e373e715091a1eb6b3830898b70307c
Guanglin Du <guanglin.du@gmail.com>
2015-01-13 14:27:30 +0800

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Ipams::Application.routes.draw do
 
   devise_for :system_users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
