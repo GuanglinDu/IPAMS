c09fbb70593482a80cc571a7db46e4ab6a39e71c
Guanglin Du <guanglin.du@gmail.com>
2015-02-03 09:39:14 +0800

    Deploy with Capistrano cont'd

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,8 @@
 lock '3.3.5'
 
 set :application, 'ipams'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :deploy_user, 'wlz'
+set :repo_url, 'git@github.com:guanglindu/ipams.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
@@ -12,6 +13,13 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default value for :scm is :git
 set :scm, :git
+
+# set rvm
+#set :rbenv_type, :system
+set :rbenv_type, :user
+set :rbenv_ruby, '2.1.1'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -23,10 +31,10 @@ set :scm, :git
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -34,7 +42,34 @@ set :scm, :git
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
+# what specs should be run before deployment is allowed to
+# continue, see lib/capistrano/tasks/run_tests.cap
+set :tests, []
+
 namespace :deploy do
+  # make sure we're deploying what we think we're deploying
+  #before :deploy, "deploy:check_revision"
+  # only allow a deploy with passing tests to deployed
+  #before :deploy, "deploy:run_tests"
+  # compile assets locally then rsync
+  #after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  #after :finishing, 'deploy:cleanup'
+
+  # remove the default nginx configuration as it will tend
+  # to conflict with our configs.
+  #before 'deploy:setup_config', 'nginx:remove_default_vhost'
+
+  # reload nginx to it will pick up any modified vhosts from
+  # setup_config
+  #after 'deploy:setup_config', 'nginx:reload'
+
+  # Restart monit so it will pick up any monit configurations
+  # we've added
+  #after 'deploy:setup_config', 'monit:restart'
+
+  # As of Capistrano 3.1, the `deploy:restart` task is not called
+  # automatically.
+  #after 'deploy:publishing', 'deploy:restart'
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
@@ -42,6 +77,13 @@ namespace :deploy do
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute "service thin restart"  ## -> line you should add
     end
   end
 
