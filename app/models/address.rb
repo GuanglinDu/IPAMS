3aebdf0ec043d95344b9e01c916ba1f4b2694e58
Guanglin Du <guanglin.du@gmail.com>
2015-02-16 08:31:37 +0800

    Init vlans

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ActiveRecord::Base
   validates :ip, presence: true, uniqueness: true
-  validates :mac_address, uniqueness: true
+  validates :mac_address, uniqueness: true, allow_blank: true
 
   belongs_to :vlan
   belongs_to :user
