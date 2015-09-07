9b38c7f18573117da4bee466ee2e61e958a8765e
Guanglin Du <guanglin.du@gmail.com>
2015-09-07 16:33:20 +0800

    Add role root to perform destroy action

--- app/models/system_user.rb
+++ app/models/system_user.rb
@@ -4,7 +4,7 @@
 # http://stackoverflow.com/questions/22213152/where-is-user-admin-defined-in-rails-devise-pundit-starter-app
 class SystemUser < ActiveRecord::Base
   # nobody: 0, guest: 1, operator: 2, expert: 3, admin: 4 
-  enum role: [:nobody, :guest, :operator, :expert, :admin] # ~> Rails 4.1
+  enum role: [:nobody, :guest, :operator, :expert, :admin, :root] # ~> Rails 4.1
 
   after_initialize :set_default_role, :if => :new_record?
 
