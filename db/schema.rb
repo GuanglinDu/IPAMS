d8607e4eabb6cec243336df9a70f129b6da2c441
Guanglin Du <guanglin.du@gmail.com>
2018-07-04 14:03:21 +0800

    Fix the default integer is 4 bytes bug
    As in SQLite3, there's no way to alter the attribute type. The
    migration change_phone_number_type_to_string cannot be applied due
    to the out of range error as the original phone numbers are in
    4-byte integer by default. The following tricks are used to overcome
    this issue:
      echo '.dump' | sqlite3 production.sqlite3 > t.dump
    Modify the office_phone and cell_phone attribute type in t.dump from
    integer to varchar(8), the string format. And then
      cat t.dump | sqlite3 p.sqlite3
    Override production.sqlite3:
      cp -f p.sqlite3 production.sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -14,46 +14,46 @@
 ActiveRecord::Schema.define(version: 20180703061744) do
 
   create_table "addresses", force: :cascade do |t|
-    t.string   "ip"
+    t.string   "ip",               limit: 255
     t.text     "usage"
     t.datetime "start_date"
     t.datetime "end_date"
-    t.string   "application_form"
+    t.string   "application_form", limit: 255
     t.integer  "vlan_id"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "mac_address"
-    t.string   "assigner"
-    t.boolean  "recyclable",       default: true, null: false
+    t.string   "mac_address",      limit: 255
+    t.string   "assigner",         limit: 255
+    t.boolean  "recyclable",                   default: true, null: false
   end
 
   add_index "addresses", ["user_id"], name: "index_addresses_on_user_id"
   add_index "addresses", ["vlan_id"], name: "index_addresses_on_vlan_id"
 
   create_table "admins", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "role",                   default: 0
-    t.string   "name"
+    t.integer  "role",                               default: 0
+    t.string   "name",                   limit: 255
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
   create_table "departments", force: :cascade do |t|
-    t.string   "dept_name"
-    t.string   "location"
+    t.string   "dept_name",  limit: 255
+    t.string   "location",   limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -62,16 +62,16 @@ ActiveRecord::Schema.define(version: 20180703061744) do
     t.text     "usage"
     t.datetime "start_date"
     t.datetime "end_date"
-    t.string   "application_form"
+    t.string   "application_form", limit: 255
     t.integer  "address_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "mac_address"
-    t.string   "user_name"
-    t.string   "dept_name"
+    t.string   "mac_address",      limit: 255
+    t.string   "user_name",        limit: 255
+    t.string   "dept_name",        limit: 255
     t.string   "office_phone",     limit: 8
     t.string   "cell_phone",       limit: 8
-    t.string   "building"
+    t.string   "building",         limit: 255
     t.integer  "room"
   end
 
@@ -79,14 +79,14 @@ ActiveRecord::Schema.define(version: 20180703061744) do
 
   create_table "lans", force: :cascade do |t|
     t.integer  "lan_number"
-    t.string   "lan_name"
+    t.string   "lan_name",        limit: 255
     t.text     "lan_description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "reserved_addresses", force: :cascade do |t|
-    t.string   "ip"
+    t.string   "ip",          limit: 255
     t.text     "description"
     t.integer  "vlan_id"
     t.datetime "created_at"
@@ -96,28 +96,28 @@ ActiveRecord::Schema.define(version: 20180703061744) do
   add_index "reserved_addresses", ["vlan_id"], name: "index_reserved_addresses_on_vlan_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",          limit: 255
     t.string   "office_phone",  limit: 8
     t.string   "cell_phone",    limit: 8
-    t.string   "email"
-    t.string   "building"
+    t.string   "email",         limit: 255
+    t.string   "building",      limit: 255
     t.integer  "storey"
     t.integer  "room"
     t.integer  "department_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title"
+    t.string   "title",         limit: 255
   end
 
   add_index "users", ["department_id"], name: "index_users_on_department_id"
 
   create_table "vlans", force: :cascade do |t|
     t.integer  "vlan_number"
-    t.string   "vlan_name"
-    t.string   "subnet_mask"
-    t.string   "gateway"
-    t.string   "static_ip_start"
-    t.string   "static_ip_end"
+    t.string   "vlan_name",        limit: 255
+    t.string   "subnet_mask",      limit: 255
+    t.string   "gateway",          limit: 255
+    t.string   "static_ip_start",  limit: 255
+    t.string   "static_ip_end",    limit: 255
     t.text     "vlan_description"
     t.integer  "lan_id"
     t.datetime "created_at"
