8745b071328e2cecbfdacfa54962220b298449f7
Guanglin Du <guanglin.du@gmail.com>
2019-07-04 08:42:03 +0800

    Show VLAN ID in users#index

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -3,7 +3,11 @@ module AddressesHelper
     Address.find(id)
   end
 
-  def self.find_nobody_id()
+  def self.find_nobody_id
     nobody_id = User.find_by(name: "NOBODY").id
+  end
+
+  def self.find_vlan_number(address)
+    Vlan.find(address.vlan_id).vlan_number
   end
 end
--- test/helpers/addresses_helper_test.rb
+++ test/helpers/addresses_helper_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class AddressesHelperTest < ActionView::TestCase
+  setup do
+    @address = addresses(:one)    
+  end
+
   test "should find address" do
-    address = addresses(:one)
-    assert_equal address, AddressesHelper.find_address(address.id)
+    assert_equal @address, AddressesHelper.find_address(@address.id)
+  end
+
+  test "should find nobody id" do
+    id = AddressesHelper.find_nobody_id
+    assert id.is_a?(Integer)
+  end
+
+  test "should find vlan number" do
+    vlan_number = AddressesHelper.find_vlan_number(@address)
+    assert_equal 1, vlan_number, "VLAN number should be 1"
   end
 end
